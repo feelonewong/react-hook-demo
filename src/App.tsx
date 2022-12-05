5f4b8f2f5df92a6fef4306619d8e36600f80e4ec
feelonewong <feelonewong@gmail.com>
2022-12-05 20:57:59 +0800

    update:06路由重定向

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {HashRouter, Route, Routes, Link} from 'react-router-dom'
+import {HashRouter, Route, Routes, Link, Navigate} from 'react-router-dom'
 import Layout from "./views/Layout/"
 import Home from "./views/Home/"
 import User from "./views/User/"
@@ -7,6 +7,7 @@ function App() {
   return (
     <HashRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/layout" />}></Route>
         <Route path='/layout' element={<Layout/>}>
           <Route index element={<Home></Home>}></Route>
           <Route path="user" element={<User></User>}></Route>
